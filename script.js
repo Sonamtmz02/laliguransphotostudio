fd56be0aa6a85369bf79cdb43b19bd73f29c0564
Sonamtmz02 <sonamt9748267824@gmail.com>
2026-08-27 08:03:52 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 /* LALIGURANS USER PANEL - realtime public site */
 const firebaseConfig = {
-  apiKey: "PASTE_YOUR_API_KEY",
-  authDomain: "PASTE_YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "PASTE_YOUR_PROJECT_ID",
-  storageBucket: "PASTE_YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "PASTE_YOUR_MESSAGING_SENDER_ID",
-  appId: "PASTE_YOUR_APP_ID"
+  apiKey: "AIzaSyAopefoW6m7RYV_HkN1rzHqMsN4tN0HJ8I",
+  authDomain: "laligurans-photo-studio.firebaseapp.com",
+  projectId: "laligurans-photo-studio",
+  storageBucket: "laligurans-photo-studio.firebasestorage.app",
+  messagingSenderId: "826817572339",
+  appId: "1:826817572339:web:8d2d2c8831c7a511b7f686"
 };
 const CUR = "रु.";
 const DEFAULT_TAG = "सम्झनाको लागी फोटो, फोटोको लागी गुराँस";
